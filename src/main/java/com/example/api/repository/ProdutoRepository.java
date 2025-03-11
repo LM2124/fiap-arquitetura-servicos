823353534fac3362eca96dc7988bdd04ab00ff9b
LM2124 <134977727+LM2124@users.noreply.github.com>
2025-03-11 11:32:48 -0300

    Aula 2025.03.11

---
+++ src/main/java/com/example/api/repository/ProdutoRepository.java
@@ -0,0 +1,6 @@
+package com.example.api.repository;
+
+import com.example.api.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {}
